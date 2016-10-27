35d57a7c7c44d7a04326b79938c436a5d30724e0
Falk Herwig <fherwig@uvic.ca>
2016-10-27 01:40:31 -0700

    add nugridpy as dependency and change name back to ppm.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "ppmpy",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ["numpy", "setuptools"],
+    install_requires = ["numpy", "setuptools","nugridpy"],
     author = "Falk Herwig, Sam Jones, Robert Andrassy, Daniel Alexander Bertolino Conti ",
     author_email = "fherwig@uvic.ca")
