44d8d3c29914b6c75e732b0517383bee4dec8b63
Falk Herwig <fherwig@uvic.ca>
2016-10-27 03:19:55 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ppmpy",
-    version = "0.21",
+    version = "0.22",
     packages = find_packages(),
     install_requires = ["numpy", "setuptools","nugridpy"],
     author = "Falk Herwig, Sam Jones, Robert Andrassy, Daniel Alexander Bertolino Conti ",
