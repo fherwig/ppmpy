d98067deccac33eb59d5e11e919722745f78d983
Falk Herwig <fherwig@uvic.ca>
2016-10-26 23:59:30 -0700

    rename and add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ppmpy",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ["numpy", "setuptools"],
+    author = "Falk Herwig, Sam Jones, Robert Andrassy, Daniel Alexander Bertolino Conti ",
+    author_email = "fherwig@uvic.ca")
